055787fbd79f70ca9d68e25d19b8c5ea8e87be93
Harshit Kumar Sharma <harshitndj15@gmail.com>
2025-04-21 05:49:20 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,4 +20,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(typeof(stringNumber));
+//console.log(typeof(stringNumber));
+
+// ************ Operations ************
+
+let value = 3;
+let negVal = -value;
+//console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " harshit"; 
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log(Number("1") + 2);
+// console.log(1+2+2+3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
