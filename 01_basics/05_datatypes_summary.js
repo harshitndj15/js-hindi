15c835690fe02fb40f97212014c13a0f264dc78b
Harshit Kumar Sharma <harshitndj15@gmail.com>
2025-04-22 03:27:05 +0000

    change in datatype summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,23 @@
+/* Primitive Datatype
+   7 types : String, Number, Boolean, null,
+             undefined, Symbol, BigInt */
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+//console.log(id === anotherId);
+
+const bigNumber = 345678909876543345678n;
+
+
+
+/* Non-Primitive (Reference Type)
+    Array, Objects, Functions
+*/
